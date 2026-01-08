cb455d44aa265fb11835e2fc1cf63470396b4d1a
Soumya Ranjan Sahoo <soumyasahoo2696@gmail.com>
2026-01-08 15:09:50 +0000

    datatype

---
+++ 00_basics/02-datatype.js
@@ -0,0 +1,32 @@
+"use strict" // use as anew version of javascript//
+
+//alert(3+3)// we are using the node not a brawser //
+
+console.log(3+3)
+
+
+console.log("soumya");
+
+
+let name= "piku"
+let age = 18
+let isloggedin = false
+ //let state= null//
+
+
+ //datatype of javascript//
+ // number=>2 the power 53
+ // bignit
+ //string=>null
+ //Boolean=>false/true 
+ // undefined//
+ // symbol =>unique
+ //object =.
+
+ console.log(typeof undefined);//undefined
+ console.log(typeof null);// object
+
+
+
+
+
