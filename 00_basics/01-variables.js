8161a6fa6ec225beaca0181cf51028be6799e4eb
Soumya Ranjan Sahoo <soumyasahoo2696@gmail.com>
2026-01-08 15:13:58 +0000

    variables

---
+++ 00_basics/01-variables.js
@@ -0,0 +1,33 @@
+const accountId = 11234;
+let accountgmail= "sahoo@gmail.com"
+var accountpassword= 12345;
+accountcity = "bangaluru"
+ console.log(accountId,accountgmail,accountpassword,accountcity);
+
+
+//accountId = 23446//
+ accountpassword="56789"
+ accountgmail="hr@gmail.com"
+ accountcity="jajpur"
+
+
+
+ console.log (accountId);
+
+ console .table([accountId,accountgmail,accountpassword,accountcity])
+
+
+
+ /*
+prefer to not use the var 
+because of issue functional scope and block scope !
+
+
+ */
+
+
+
+
+
+
+
