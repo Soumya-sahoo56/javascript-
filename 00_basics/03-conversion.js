a01ad39dadef7e96758514194a45126ba9460842
Soumya Ranjan Sahoo <soumyasahoo2696@gmail.com>
2026-01-09 14:27:11 +0000

    comparision

--- 00_basics/03-conversion.js
+++ 00_basics/03-conversion.js
@@ -17,3 +17,29 @@ let somenumber =33
 let stringnumber = String(somenumber)
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+
+
+
+//******************************operations**************************/
+
+let value =3
+let negvalue = -value
+console.log (value);
+
+
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2%2);
+//console.log(2/2);
+//console.log(2%2);
+//console.log(2*2);
+
+
+let string1= soumya
+let string2= sahoo
+console.log(sring1+string2);
+
+
