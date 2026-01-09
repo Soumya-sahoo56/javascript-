a0965451a831ec7edd627f2530383c187be3edfc
Soumya Ranjan Sahoo <soumyasahoo2696@gmail.com>
2026-01-09 13:49:31 +0000

    conversion & datatype

---
+++ 00_basics/03-conversion.js
@@ -0,0 +1,19 @@
+let score =34
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber = number(score)
+console.log (typeof valueinnumber);
+//console.log(valueinnumber);
+
+
+//i=> true; 0=>flase
+//"=>flase
+// //"soumya "=> true 
+
+let somenumber =33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
