f20114553ee2c37f64c34ee47ca95dde13512b04
Soumya Ranjan Sahoo <soumyasahoo2696@gmail.com>
2026-01-09 15:08:31 +0000

    datatype summerry

---
+++ 00_basics/datatypesummery.js
@@ -0,0 +1,16 @@
+// there are two tyoe of data type are avaliable one is primituve or non primitive 
+// 
+
+// console.log(2);
+//  if {
+//     range is =
+//  }
+
+
+const isloggedin = false
+const outsideTemp = null 
+let usermail;
+
+const id = Symbol('123')
+const anither id =Symbol('123')
+console.log(id === anatherid);
