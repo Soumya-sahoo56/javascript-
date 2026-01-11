c794fb8ca707a69ed5e203a05e0d875bf2cbdcc4
Soumya Ranjan Sahoo <soumyasahoo2696@gmail.com>
2026-01-11 13:15:10 +0000

    math-num

---
+++ 00_basics/06-num-math.js
@@ -0,0 +1,28 @@
+const score=400
+console.log("score");
+
+
+
+const balance = new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(1));
+
+console.log(Math.min(4,3,5,1));
+console.log(Math.min(3,4,1,0,));
+console.log(Math.random());
+console.log(math.random()*10 +1);
+console.log(math.floor(math.random()*10));
+
+const min =20
+const max = 30
+console.log(math.random()*(max-min +1))
+
+
+
+
+
+
