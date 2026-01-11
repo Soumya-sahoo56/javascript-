ac78334723d52b4a8f3dc0fd90e78b43aed6f5e3
Soumya Ranjan Sahoo <soumyasahoo2696@gmail.com>
2026-01-11 14:12:47 +0000

    date.and time

---
+++ 00_basics/07-date.js
@@ -0,0 +1,9 @@
+let mydate = new date();
+console.log(mydate.tostring());
+console.log(mydate.letstring());
+console.log(mydate.newstring());
+console.log(mydate.tostring());
+console.log(typeof mydate);
+
+
+
